b948ff30dc0cf019c589cf884a54735a5b78509c
Thuc Le Duy <thuc.leduy.int@gmail.com>
2019-09-10 00:08:43 +0700

    Add Service and IServiceManager

---
+++ CoreDD/Models/IServiceManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreDD.Models
+{
+    interface IServiceManager
+    {
+        bool AddServiceWithIdInvoice(int idInvoice, Service service);
+        bool DeleteServiceWithIdInvoice(int idInvoice, int idService);
+    }
+}
